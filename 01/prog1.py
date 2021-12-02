a783b636b9047e4f2bd2b48ec494b629752bff13
Olle Mulmo <olle@mulmo.net>
2021-12-02 10:54:35 +0100

    shorter, but is it improved?

--- 01/prog1.py
+++ 01/prog1.py
@@ -2,10 +2,12 @@ import sys
 
 inputs=[int(x) for x in sys.stdin]
 
-bigger=0
-for i in range(1,len(inputs)):
-    if inputs[i]>inputs[i-1]:
-        bigger += 1
+#bigger=0
+#for i in range(1,len(inputs)):
+#    if inputs[i]>inputs[i-1]:
+#        bigger += 1
+#
+#print(bigger)
 
-print(bigger)
+print(sum(map(lambda x: x[1]>x[0], [inputs[i:i+2] for i in range(0,len(inputs)-1)])))
 
