48f9861113e005a20b1f194d1b08f4c8733d1cb4
Olle Mulmo <olle@mulmo.net>
2021-12-09 09:53:27 +0100

    Cleaned things up a bit

--- 08/star2.py
+++ 08/star2.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from functools import reduce
 import sys
 
 DIGITS = [ 'abcefg', 'cf', 'acdeg', 'acdfg', 'bcdf', 'abdfg', 'abdefg', 'acf', 'abcdefg', 'abcdfg' ]
@@ -15,24 +16,23 @@ def analyze(signals, digits):
     assert(len(signals)==10)
     assert(len(digits)==4)
     signals = sorted(signals, key=len)
-    setstr = lambda x: ''.join(sorted(x))
 
     # 1,4,7,8 = known because of unique no. of segments
     # 2,3,5 = 5 segments
     # 0,6,9 = 6 segments
-
-    one = set(signals[0])
-    seven = set(signals[1])
-    four = set(signals[2])
-    eight = set(signals[9])
+    one = signals[0]
+    seven = signals[1]
+    four = signals[2]
+    eight = signals[9]
 
     a = seven.difference(one)
     bd = four.difference(one)
     eg = eight.difference(seven).difference(bd)
 
+    # of 2,3,5 bd only exist in 5
     five=set()
     for signal in signals[3:6]:
-        if bd.issubset(set(signal)):
+        if bd.issubset(signal):
             five = set(signal)
     assert(len(five)==5)
 
@@ -43,25 +43,25 @@ def analyze(signals, digits):
     f = one.difference(c)
     g = eg.difference(e)
 
+    # 3 = acfg + d
     acfg = a.union(c).union(f).union(g)
-
     d, three = set(), set()
     for signal in signals[3:6]:
-        diff = set(signal).difference(acfg)
+        diff = signal.difference(acfg)
         if len(diff)==1:
             d = diff
-            three = set(signal)
+            three = signal
     assert(len(three)==5)
     zero = eight.difference(d)
     two = three.union(e).difference(f)
 
-    patterns = list(map(setstr, [zero,one,two,three,four,five,six,seven,eight,nine]))
-    return int(''.join(map(lambda x: str(patterns.index(x)), digits)))
+    patterns = [zero,one,two,three,four,five,six,seven,eight,nine]
+    return reduce(lambda cur,nxt: cur*10+nxt, map(patterns.index, digits))
 
 
 sum = 0
 for line in sys.stdin:
-    signalset = list(map(lambda x: ''.join(sorted(x)), line.strip().replace(' | ', ' ').split(' ')))
+    signalset = list(map(set, line.strip().replace(' | ', ' ').split(' ')))
     sum += analyze(signalset[0:10], signalset[10:])
     
 print(sum)
